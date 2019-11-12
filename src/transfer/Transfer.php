390f3557c7f9e04fad6affb860a0fa1beaf5f011
Chawatvish Worrapoj <chawatvish.w@kbtg.tech>
2019-11-12 10:15:21 +0700

    Added transfer module file.

---
+++ src/transfer/Transfer.php
@@ -0,0 +1,13 @@
+<?php namespace Operation;
+
+require_once __DIR__ . './../serviceauthentication/DBConnection.php';
+require_once __DIR__ . './../serviceauthentication/StubServiceAuthentication.php';
+require_once __DIR__ . './../serviceauthentication/ServiceAuthentication.php';
+
+use AccountInformationException;
+use DBConnection;
+use ServiceAuthentication;
+
+class Transfer {
+    
+}
