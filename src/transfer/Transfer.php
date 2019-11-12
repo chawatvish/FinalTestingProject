e92695a96973cb0a2554e52ee2cea83bc8ede4d6
Chawatvish Worrapoj <chawatvish.w@kbtg.tech>
2019-11-12 12:44:23 +0700

    Added complete function code

--- src/transfer/Transfer.php
+++ src/transfer/Transfer.php
@@ -13,7 +13,7 @@ class Transfer
     public function __construct(string $srcNumber,
         string $srcName,
         ServiceAuthentication $service,
-        DBConnection $dbConnection ) {
+        DBConnection $dbConnection) {
 
         $this->srcNumber = $srcNumber;
         $this->srcName = $srcName;
@@ -31,28 +31,32 @@ class Transfer
             return $response;
         }
 
-        // 1----  Check toAcctNo is aready set in system
-        $result = $service::accountAuthenticationProvider($targetNumber);
-        $acctNo = $result["accNo"];
-        $toBal = floatval($result["accBalance"]);
+        try {
+            // 1----  Check toAcctNo is aready set in system
+            $result = $service::accountAuthenticationProvider($targetNumber);
+            $acctNo = $result["accNo"];
+            $toBal = floatval($result["accBalance"]);
 
-        // 2----  withdraw from scrAcctNo
-        $result = $service::accountAuthenticationProvider($this->srcNumber);
-        $srcBal = floatval($result["accBalance"]);
+            // 2----  withdraw from scrAcctNo
+            $result = $service::accountAuthenticationProvider($this->srcNumber);
+            $srcBal = floatval($result["accBalance"]);
 
-        if ($srcBal < $amount) {
-            $response["isError"] = true;
-            throw new AccountInformationException("ยอดเงินไม่เพียงพอ");
+            if ($srcBal < $amount) {
+                $response["isError"] = true;
+                throw new AccountInformationException("ยอดเงินไม่เพียงพอ");
+            }
+
+            $srcBalAfter = $srcBal - $amount;
+            $dbConnection::saveTransaction($this->srcNumber, $srcBalAfter);
+
+            $toBalAfter = $toBal + $amount;
+            $dbConnection::saveTransaction($targetNumber, $toBalAfter);
+
+            $response["isError"] = false;
+            $response["message"] = $srcBalAfter;
+        } catch (Exception $e) {
+            $response["message"] = $e->getMessage();
         }
-
-        $srcBalAfter = $srcBal - $amount;
-        $dbConnection::saveTransaction($this->srcNumber, $srcBalAfter);
-
-        $toBalAfter = $toBal + $amount;
-        $dbConnection::saveTransaction($targetNumber, $toBalAfter);
-
-        $response["isError"] = false;
-        $response["message"] = $srcBalAfter;
 
         return $response;
     }
