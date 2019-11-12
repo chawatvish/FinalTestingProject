4a4161e4210e892820ee04ca028d1a50eb0f2ff3
Chawatvish Worrapoj <chawatvish.w@kbtg.tech>
2019-11-12 16:56:38 +0700

    Added more test cases.

--- src/transfer/Transfer.php
+++ src/transfer/Transfer.php
@@ -1,8 +1,10 @@
 <?php namespace Operation;
 
+require_once __DIR__ . './../serviceauthentication/AccountInformationException.php'; 
 require_once __DIR__ . './../serviceauthentication/DBConnection.php';
 require_once __DIR__ . './../serviceauthentication/ServiceAuthentication.php';
 
+use AccountInformationException;
 use ServiceAuthentication;
 use DBConnection;
 
--- test/TransferTest.php
+++ test/TransferTest.php
@@ -11,10 +11,35 @@ use Stub\StubDBConnection;
 
 class TransferTest extends TestCase
 {
-    public function testOverTC001()
+    public function testTC001()
     {
         $transfer = new Transfer('3333333001', 'TEST 001', new StubServiceAuthentication(), new StubDBConnection());
-        $result2 = $transfer->doTransfer('3333333005', 5000);
-        $this->assertSame(false, $result2['isError']);
+        $result = $transfer->doTransfer('333333300g', 5000);
+        $this->assertTrue($result['isError']);
+        $this->assertEquals("หมายเลขบัญชีไม่ถูกต้อง", $result["message"]);
+    }
+
+    public function testTC002()
+    {
+        $transfer = new Transfer('3333333001', 'TEST 001', new StubServiceAuthentication(), new StubDBConnection());
+        $result = $transfer->doTransfer('123456789', 5000);
+        $this->assertTrue($result['isError']);
+        $this->assertEquals("หมายเลขบัญชีไม่ถูกต้อง", $result["message"]);
+    }
+
+    public function testTC003()
+    {
+        $transfer = new Transfer('3333333001', 'TEST 001', new StubServiceAuthentication(), new StubDBConnection());
+        $result = $transfer->doTransfer('123456789', 5000);
+        $this->assertTrue($result['isError']);
+        $this->assertEquals("หมายเลขบัญชีไม่ถูกต้อง", $result["message"]);
+    }
+    
+    public function testTC004()
+    {
+        $transfer = new Transfer('3333333001', 'TEST 001', new StubServiceAuthentication(), new StubDBConnection());
+        $result = $transfer->doTransfer('3333333005', 50000);
+        $this->assertTrue($result['isError']);
+        $this->assertEquals("ยอดเงินไม่เพียงพอ", $result["message"]);
     }
 }
